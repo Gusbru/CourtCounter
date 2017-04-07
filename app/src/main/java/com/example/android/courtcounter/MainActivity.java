a59a113875d2c88678152956b921ca94f57c0193
Gustavo Brunetto <gustavobruneto@gmail.com>
2017-04-07 16:04:37 -0300

    Add comments for all methods Change the reset button style

--- app/src/main/java/com/example/android/courtcounter/MainActivity.java
+++ app/src/main/java/com/example/android/courtcounter/MainActivity.java
@@ -27,50 +27,83 @@ public class MainActivity extends AppCompatActivity {
         scoreView.setText(String.valueOf(scoreTeamA));
     }
 
+    /**
+     * method to display the score for team B
+     */
     public void displayForTeamB() {
         TextView scoreView = (TextView) findViewById(R.id.scores_B);
         scoreView.setText(String.valueOf(scoreTeamB));
     }
 
     /**
-     * @param view increase 3 points when button is clicked
+     * @param view
+     *
+     * increase 3 points when button is clicked
      */
     public void increase3PointsTeamA(View view) {
         scoreTeamA += POINTS_FOR_LONG_THROW;
         displayForTeamA();
     }
 
+    /**
+     *
+     * @param view
+     *
+     * Method to increment 3 points for team B
+     */
     public void increase3PointsTeamB(View view) {
         scoreTeamB += POINTS_FOR_LONG_THROW;
         displayForTeamB();
     }
 
     /**
-     * @param view increase 2 points when button is clicked
+     * @param view
+     *
+     * increase 2 points when button is clicked
      */
     public void increase2PointsTeamA(View view) {
         scoreTeamA += POINTS_FOR_NORMAL_THROW;
         displayForTeamA();
     }
 
+    /**
+     *
+     * @param view
+     *
+     * increase 2 points for team B
+     */
     public void increase2PointsTeamB(View view) {
         scoreTeamB += POINTS_FOR_NORMAL_THROW;
         displayForTeamB();
     }
 
     /**
-     * @param view increase 1 point when button is clicked
+     * @param view
+     *
+     * increase 1 point when button is clicked
      */
     public void increase1PointsTeamA(View view) {
         scoreTeamA += POINTS_FOR_FREE_THROW;
         displayForTeamA();
     }
 
+    /**
+     *
+     * @param view
+     *
+     * increase 1 point for team B
+     */
     public void increase1PointsTeamB(View view) {
         scoreTeamB += POINTS_FOR_FREE_THROW;
         displayForTeamB();
     }
 
+    /**
+     *
+     * @param view
+     *
+     * reset the score for both teams, A and B
+     */
     public void resetScore(View view) {
         scoreTeamA = 0;
         scoreTeamB = 0;
