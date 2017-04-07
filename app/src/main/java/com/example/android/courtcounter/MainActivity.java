872786a012f3fc50afdb83fa1b8be24539b54bed
Gustavo Brunetto <gustavobruneto@gmail.com>
2017-04-07 15:03:38 -0300

    - First implementation. - Initial design - buttons are working - score is cumulative.

--- app/src/main/java/com/example/android/courtcounter/MainActivity.java
+++ app/src/main/java/com/example/android/courtcounter/MainActivity.java
@@ -2,6 +2,8 @@ package com.example.android.courtcounter;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,4 +12,43 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    int score = 0;
+
+    /**
+     *
+     * @param score display the score for team A
+     */
+    public void displayForTeamA(int score) {
+        TextView scoreView = (TextView) findViewById(R.id.scores_A);
+        scoreView.setText(String.valueOf(score));
+    }
+
+    /**
+     *
+     * @param view increase 3 points when button is clicked
+     */
+    public void increase3Points(View view) {
+        score += 3;
+        displayForTeamA(score);
+    }
+
+    /**
+     *
+     * @param view increase 2 points when button is clicked
+     */
+    public void increase2Points(View view) {
+        score += 2;
+        displayForTeamA(score);
+    }
+
+    /**
+     *
+     * @param view increase 1 point when button is clicked
+     */
+    public void increase1Points(View view) {
+        score += 1;
+        displayForTeamA(score);
+    }
+
 }
