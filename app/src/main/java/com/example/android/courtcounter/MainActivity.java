00ff484eac06171e32cba7c73486cf759e140d77
Gustavo Brunetto <gustavobruneto@gmail.com>
2017-04-07 15:11:32 -0300

    change the name of the variable score to scoreTeamA. add the scores for each throw as constants

--- app/src/main/java/com/example/android/courtcounter/MainActivity.java
+++ app/src/main/java/com/example/android/courtcounter/MainActivity.java
@@ -7,17 +7,19 @@ import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
+    final int POINTS_FOR_FREE_THROW = 1;
+    final int POINTS_FOR_NORMAL_THROW = 2;
+    final int POINTS_FOR_LONG_THROW = 3;
+    int scoreTeamA = 0;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
 
-    int score = 0;
-
     /**
-     *
-     * @param score display the score for team A
+     * @param score display the scoreTeamA for team A
      */
     public void displayForTeamA(int score) {
         TextView scoreView = (TextView) findViewById(R.id.scores_A);
@@ -25,30 +27,27 @@ public class MainActivity extends AppCompatActivity {
     }
 
     /**
-     *
      * @param view increase 3 points when button is clicked
      */
     public void increase3Points(View view) {
-        score += 3;
-        displayForTeamA(score);
+        scoreTeamA += POINTS_FOR_LONG_THROW;
+        displayForTeamA(scoreTeamA);
     }
 
     /**
-     *
      * @param view increase 2 points when button is clicked
      */
     public void increase2Points(View view) {
-        score += 2;
-        displayForTeamA(score);
+        scoreTeamA += POINTS_FOR_NORMAL_THROW;
+        displayForTeamA(scoreTeamA);
     }
 
     /**
-     *
      * @param view increase 1 point when button is clicked
      */
     public void increase1Points(View view) {
-        score += 1;
-        displayForTeamA(score);
+        scoreTeamA += POINTS_FOR_FREE_THROW;
+        displayForTeamA(scoreTeamA);
     }
 
 }
